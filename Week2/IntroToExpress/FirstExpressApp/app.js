4d94b0558ab6bcea6662c67f7c7fb8857a16212e
EC2 Default User <ec2-user@ip-172-31-5-46.us-west-2.compute.internal>
2017-12-14 17:40:11 +0000

    Updates

--- Week2/IntroToExpress/FirstExpressApp/app.js
+++ Week2/IntroToExpress/FirstExpressApp/app.js
@@ -14,6 +14,14 @@ app.get("/dog", function(req, res){
    res.send("MEOW."); 
 });
 
+app.get("/r/:subredditName", function(req, res){
+   res.send("Welcome to a subreddit"); 
+});
+
+app.get("*", function(req, res){
+    res.send("You are a star.");
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started.");
 });
