157890a3e4d60eb8d06b98c894968a3768c22e3c
Connor Truax <connortruax73@gmail.com>
2018-01-03 17:32:41 +0000

    Added Route Params

--- Week2/IntroToExpress/FirstExpressApp/app.js
+++ Week2/IntroToExpress/FirstExpressApp/app.js
@@ -2,24 +2,29 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-   res.send("Hi there!"); 
+    res.send("Hi there!"); 
 });
 
 app.get("/bye", function(req, res){
-   res.send("Goodbye."); 
+    res.send("Goodbye."); 
 });
 
 app.get("/dog", function(req, res){
     console.log("SOMEONE MADE A REQUEST TO /dog");
-   res.send("MEOW."); 
+    res.send("MEOW."); 
 });
 
 app.get("/r/:subredditName", function(req, res){
-   res.send("Welcome to a subreddit"); 
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    res.send("Welcome to the comments page")
 });
 
 app.get("*", function(req, res){
-    res.send("You are a star.");
+    res.send("Star")
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
