6f4df3793bfb81c0a7a30d2ef09a311cf7d1b3db
Connor Truax <connortruax73@gmail.com>
2018-01-05 20:54:25 +0000

    Updates

--- Week2/MoreExpress/EJSDemo/app.js
+++ Week2/MoreExpress/EJSDemo/app.js
@@ -2,7 +2,12 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-    res.send("<h1>Welcome</h1>")
+    res.render("home.ejs")
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
