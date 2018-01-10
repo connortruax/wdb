38855614015d1294d1c4d51c3b9d7205b44d19f5
Connor Truax <connortruax73@gmail.com>
2018-01-10 17:26:41 +0000

    Added gitignore

--- Week2/MoreExpress/EJSDemo/app.js
+++ Week2/MoreExpress/EJSDemo/app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
     res.render("home.ejs")
 });
@@ -8,6 +11,15 @@ app.get("/", function(req, res){
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Suzy"},
+        {title: "My adorable pet bunny", author: "Charlie"},  
+        {title: "Can you believe this pomsky", author: "Colt"}    
+    ];
+    res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
